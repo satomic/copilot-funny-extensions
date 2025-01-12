474c883083bb229ad7c1eb724307c2d00bc81189
satomic <yxfdental@126.com>
2025-01-12 16:15:44 +0800

    refactor: update Dockerfile and build script to use dynamic APP_FOLDER variable

--- skillset-website-updater/docker_build.sh
+++ skillset-website-updater/docker_build.sh
@@ -1,1 +1,3 @@
-docker build -t satomic/skillset:`date +'%Y%m%d'` .
+cd ..
+
+docker build --build-arg APP_FOLDER=skillset-website-updater -t satomic/skillset:`date +'%Y%m%d'` -f skillset-website-updater/Dockerfile .
